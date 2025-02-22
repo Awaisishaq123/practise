720f458d8872c5ce2f26af3d403f1095ac329c24
Awaisishaq123 <awaisishaq003@gmail.com>
2025-02-22 23:44:27 +0000

    initail commit

---
+++ js practise/aap.js
@@ -0,0 +1,49 @@
+// let num1=5
+// let num2=6
+ 
+
+
+// let res = num2++ + ++num1 - --num1
+//             6 + 6 -5
+
+ 
+
+
+
+
+// for(var i=10; i>=0; i--){
+
+//     document.write(i +" " )
+// } 
+
+
+
+
+// // indexof array per bhi chlata hai aur string mai bhin chalta hai 
+
+// var str ='saylani'
+// document.write(alert("hello"))
+
+
+
+// let num = 5 +6
+// let num4 = 5/67
+// let num3 = 5%7
+// let num5 = 5%7
+// let num6 ,= 5-7
+// let num8 = 5*68
+
+
+// let mix = num+ num4 +num3+num5+ num6+num8
+// // Math.random()
+
+
+// document.write(mix)
+
+var result = 10 + 2 * 5 /1234 *123;
+console.log( result);
+// for (i=1;  i<=10; i++){
+//     for(j=1; j<=10; j++)
+//         continue
+//     console.log("number :" + i)
+// }
---
+++ youtube tutorial js/class1 ,data types/app.js
@@ -0,0 +1,65 @@
+// const procuct = {
+//     title: "ball pen",
+//     rating : 5,
+//     offer :5,
+//     price :270
+// };
+
+// // console.log(procuct)
+
+
+
+// const profile ={
+//     title: "shradhakhapra",
+//     followers :569,
+//     following :4,
+//     post : 195,
+//     isFollow :false 
+// };
+// console.log(typeof profile ["title"]);
+
+
+
+// let num1 = 5;
+// let num2 = 10;
+
+// console.log(num1*num2)
+// console.log("a+b =",num1+num2)
+// console.log("a-b =",num1-num2)
+// console.log("a*b =",num1*num2)
+// console.log("a/b =",num1/num2)
+
+
+// console.log("a%b =",num1%num2)
+// // modulus operator
+// console.log("a**b =",num1**num2)     
+// // exponential operator
+
+
+// let num1 = 5;
+// let num2 = "10";
+
+
+
+// console.log("5>10", num1>num2);
+
+// console.log( "10 <5", num1<num2);
+
+
+// console.log("5===10" , num1===num2 )
+// console.log("5===5" , num1===num2 )
+
+
+
+
+// logical operator
+
+
+let a = 6;
+let b = 5;
+
+let cond1 = a>b;
+let cond2 = a ===b;
+
+document.write("cond1 || cond2 =" , cond1 || cond2);
+
