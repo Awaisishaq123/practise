ebeca114530e994f887d900b640bb590d4b50464
Awaisishaq123 <awaisishaq003@gmail.com>
2025-02-25 23:45:17 +0000

    update

--- js practise/aap.js
+++ js practise/aap.js
@@ -1,12 +1,12 @@
 // let num1=5
 // let num2=6
- 
+
 
 
 // let res = num2++ + ++num1 - --num1
 //             6 + 6 -5
 
- 
+
 
 
 
@@ -54,12 +54,137 @@
 // var eq = ((2 % 4) * 4) + 100;
 // console.log(eq)\
 
-var array =["ali","asad","aliyan","imran","kasaan","hafiz"]
 // array.push("muhammad ")
 // array.pop()
 // array.unshift("hammad")
 // array.shift(1234)
 // array.splice(0,0,"emaan")
-array.slice(2,3)
+// array.slice(2,3)
 
-console.log(array)
+// console.log(array)
+
+// var array =["ali","asad","hafiz","aliyan","imran", "asad","kasaan","hafiz"]
+// document.write(array.lastIndexOf("hafiz"))
+
+
+// let matrix = [
+//     [1, 2, 3], 
+//     [4, 5, 6], 
+//     [7, 8, 9]
+// ];
+// document.write(matrix[1][2])
+
+// let dat = new date()
+// console.log(dat)
+
+
+
+// conditional operator
+
+// let mode = "black"
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode === "blue") {
+//     color = "white"
+// }
+// else if (mode === "pink") {
+//     color = "pink";
+// } else {
+//     color = "white";
+// }
+// alert(color)
+
+
+
+// ternary opertor
+// let age =+prompt("enter your age");
+
+// let result= age >= 18 ? "adult" :"not adult";
+// alert(result)
+
+
+
+// let score =prompt("enter your grades");
+// let  grade;
+// if(score >=90 && score <= 100){
+//     grade = "A+";
+// }
+// else if(score >= 70 && score <=89){
+//     grade = "A"
+// }
+// else if(score >= 60 && score <=69){
+//     grade = "B"
+// }
+
+// else if(score >= 50 && score <=59){
+//     grade = "C"
+    
+// }
+// else if(score >=0 && score<= 49)(
+//     grade = "F"
+// )
+
+// console.log("according to your scores your  grade was :"+ grade )
+
+// while loop 
+
+
+
+// let i =100;
+// while(i >= 101){
+//     document.write(i + "<br>")
+//     i--
+// }
+
+
+// do while loop
+
+
+
+// let i = 55;
+// do {
+//     document.write(i +"<br>");
+//     i--;
+// }while (i>=56);
+
+
+
+
+// for loop 
+
+
+
+// for (i=5;  i>=1; i--){
+//     document.write(i +"<br>")
+// }
+
+// for-of loop
+
+
+
+// let str ="hello manahil"
+
+// for (let i of str){
+//     document.write (i);
+// }
+
+// for in loop
+
+// let student ={
+//     name :"manahil",
+//     age :"18th",
+//     ispass:true,
+
+// };
+
+// for (let key in student){
+// console.log(key,"value =", student[key]);
+// }
+
+for(let i=0 ; i<=100; i++){
+    if(i%2 ===0){
+        document.write("i=",i +"<br>")
+    }
+} 
