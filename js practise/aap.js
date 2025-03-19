d58b1dfb9587f2a82a190da4b77ee45e105bc8e2
Awaisishaq123 <awaisishaq003@gmail.com>
2025-03-19 17:53:12 +0000

    updated

--- js practise/aap.js
+++ js practise/aap.js
@@ -1,30 +1,18 @@
 // let num1=5
 // let num2=6
 
-
-
 // let res = num2++ + ++num1 - --num1
 //             6 + 6 -5
-
-
-
-
-
 
 // for(var i=10; i>=0; i--){
 
 //     document.write(i +" " )
-// } 
+// }
 
-
-
-
-// // indexof array per bhi chlata hai aur string mai bhin chalta hai 
+// // indexof array per bhi chlata hai aur string mai bhin chalta hai
 
 // var str ='saylani'
 // document.write(alert("hello"))
-
-
 
 // let num = 5 +6
 // let num4 = 5/67
@@ -33,10 +21,8 @@
 // let num6 ,= 5-7
 // let num8 = 5*68
 
-
 // let mix = num+ num4 +num3+num5+ num6+num8
-// // Math.random()
-
+// Math.random()
 
 // document.write(mix)
 
@@ -47,9 +33,6 @@
 //         continue
 //     console.log("number :" + i)
 // }
-
-
-
 
 // var eq = ((2 % 4) * 4) + 100;
 // console.log(eq)\
@@ -65,19 +48,15 @@
 
 // var array =["ali","asad","hafiz","aliyan","imran", "asad","kasaan","hafiz"]
 // document.write(array.lastIndexOf("hafiz"))
-
-
 // let matrix = [
-//     [1, 2, 3], 
-//     [4, 5, 6], 
+//     [1, 2, 3],
+//     [4, 5, 6],
 //     [7, 8, 9]
 // ];
 // document.write(matrix[1][2])
 
 // let dat = new date()
 // console.log(dat)
-
-
 
 // conditional operator
 
@@ -96,15 +75,11 @@
 // }
 // alert(color)
 
-
-
-// ternary opertor
+// // ternary opertor
 // let age =+prompt("enter your age");
 
 // let result= age >= 18 ? "adult" :"not adult";
 // alert(result)
-
-
 
 // let score =prompt("enter your grades");
 // let  grade;
@@ -120,7 +95,7 @@
 
 // else if(score >= 50 && score <=59){
 //     grade = "C"
-    
+
 // }
 // else if(score >=0 && score<= 49)(
 //     grade = "F"
@@ -128,9 +103,7 @@
 
 // console.log("according to your scores your  grade was :"+ grade )
 
-// while loop 
-
-
+// while loop
 
 // let i =100;
 // while(i >= 101){
@@ -138,10 +111,7 @@
 //     i--
 // }
 
-
 // do while loop
-
-
 
 // let i = 55;
 // do {
@@ -149,20 +119,13 @@
 //     i--;
 // }while (i>=56);
 
-
-
-
-// for loop 
-
-
+// for loop
 
 // for (i=5;  i>=1; i--){
 //     document.write(i +"<br>")
 // }
 
 // for-of loop
-
-
 
 // let str ="hello manahil"
 
@@ -183,8 +146,63 @@
 // console.log(key,"value =", student[key]);
 // }
 
-for(let i=0 ; i<=100; i++){
-    if(i%2 ===0){
-        document.write("i=",i +"<br>")
-    }
-} 
+// for(let i=0 ; i<=100; i++){
+//     if(i%2 ===0){
+//         document.write("i=",i +"<br>")
+//     }
+// }
+
+// function str(){
+//     console.log(str)
+// // }
+
+// function reverseString(str) {
+//     // Your code here
+// }
+// console.log(reverseString("hello"));
+
+// function ispalindrome(str){
+
+// }
+
+// console.log(ispalindrome('madam'))
+// console.log(ispalindrome('hello'))
+
+// function factorial(n) {
+//     let fact = +prompt("enter your factorial");
+//     for (let i = 1; i <= n; i++) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+// console.log(factorial(5));
+
+// 21
+// let str ="hello"
+// let capital = str.charAt(0).toUpperCase() +str.slice(1).toLowerCase();
+// document.writeln(capital)
+
+// 22
+// let str ="owais"
+
+// let final = (str.length)
+// document.writeln(final)
+
+// 23
+// let para = Text.toString().slice()
+// console.log(para)
+
+
+
+// const firstChar = cityToCheck.slice(0, 1);
+// console.log(firstChar)
+
+
+// let index = "helloworld"
+
+// let final = index.indexOf("hello" ,)
+
+// console.log(final)
+let text = "Hello world, welcome to the world!";
+let index1= text.lastIndexOf("world");
+console.log(index1); // 23
